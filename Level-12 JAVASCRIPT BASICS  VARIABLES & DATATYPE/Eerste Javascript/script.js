24ad9b013ce218b5a902e57aed7edd0018dd9420
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-01 10:07:58 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/script.js
@@ -0,0 +1,22 @@
+console.log("Hello Winc Academy")
+let firstName = 'Zafar';
+console.log(firstName);
+console.log(4+4);//Addition +
+console.log('4'+'4');
+//Maak nog wat simpele sommetjes met de volgende operators en log* de uitkomst naar de console. Gebruik:
+console.log(12-4);//Voorbeeld: Subtraction -
+console.log(5*9);//Voorbeeld: Multiplication *
+console.log(45/5);//Voorbeeld: Division /
+console.log(12 % 5);//Voorbeeld: Modulus %
+let lastName = 'Baig';
+let age = 35;
+console.log(lastName, age);
+//Typeof
+let name = 'Zafar';
+let postalCode = 4581 ;
+let isApproved = false;
+let streetName = undefined;
+let selectColor = null;
+console.log(postalCode);
+
+
