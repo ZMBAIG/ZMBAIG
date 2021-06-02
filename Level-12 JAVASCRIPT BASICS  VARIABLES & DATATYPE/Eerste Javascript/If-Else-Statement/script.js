e1a189a450d554996a00211ddfdea9221ba57d72
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-02 17:46:06 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/If-Else-Statement/script.js
@@ -0,0 +1,22 @@
+
+//Deel-01:
+const age =8;
+    if (age >= 18){
+        console.log("Je mag naar binnen komen!");
+    } else if (age <18){
+        console.log("Te jong! je mag niet naar binnen komen.");
+    }
+//Deel-02:
+const isFemale = true;
+    if (isFemale){
+        console.log("is waar")
+    }else {(isFemale)
+        console.log("is niet")
+    }
+//Deel-03:
+const driverStatus = 'gedronk';
+    if (driverStatus){
+    console.log("Bob heeft en mag rijden")
+    }else {(driverStatus)
+    console.log("geen Bob is en dus niet mag rijden")
+}
