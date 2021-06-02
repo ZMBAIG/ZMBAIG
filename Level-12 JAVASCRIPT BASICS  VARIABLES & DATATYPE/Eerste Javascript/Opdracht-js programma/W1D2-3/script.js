fc8ec2732a7f3865c3dc35241b128c1b4c7f7238
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-02 09:52:42 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/Opdracht-js programma/W1D2-3/script.js
@@ -0,0 +1,17 @@
+//Deel 1:
+
+console.log("Hallo Winc Academy studenten")
+//Deel-02
+
+//"Dit is een grote som".
+let multiplication = 1230941 * 1823794;
+console.log("multiplication =", multiplication);
+let division = 637263 / 54;
+console.log("division=", division);
+
+/*let multiplication = 1230941 * 1823794;
+console.log("multiplication =", multiplication);
+let division = 637263 / 54;
+console.log("division=", division);*/
+/*als een comment gebruik dan uitkomst is alleen de syntax in de code komt. 
+Een comment staat, doet niet meer mee voor de JavaScript engine.*/
