877d82306beed1a0eb91fca48304ce4eb684ac01
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-03 10:16:18 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/Comparison && Logical Operators/script.js
@@ -0,0 +1,22 @@
+
+//Deel 1: 50% korting:
+const age =18;
+    if (age >= 18 && age <= 25){
+        console.log("Je krijgt 50% korting!")
+    } else if (age <18){
+        console.log("Het is niet waar.");
+    }
+ //Deel 2: Ludieke actie! 
+ const name =  "Sarah";  
+    if (name == "Sarah" ||name =="Abraham"){
+        console.log(name);
+    }
+//Deel 3: Jubileum korting 
+const totalAmount = 100;
+    if (totalAmount >25) {
+        console.log("Dan krijg je gratis (vega)bitterballen.");
+    } else if (totalAmount >50){
+        console.log("Dan krijg je gratis portie nachos.");
+    } else (totalAmount >= 100);{
+        console.log("Dan krijg je een gratis flesje champagne.")
+    }   
