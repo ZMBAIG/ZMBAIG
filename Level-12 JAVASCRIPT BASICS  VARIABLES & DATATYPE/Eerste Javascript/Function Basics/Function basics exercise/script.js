66dc91c7e12fcfbe2278e3be876d8f1102c93085
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-05 14:27:07 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/Function Basics/Function basics exercise/script.js
@@ -0,0 +1,23 @@
+function makePizza (name){
+    console.log("Spaghetti","Funghi");
+}
+
+function makeSushi(name){
+  console.log("Sushi");    
+}
+//now call your function three times
+makePizza(name);
+makePizza(name);
+makePizza(name);
+//add a function that makes sushi,
+makeSushi(name);
+/*So now, let's for a minute pretend that we work in a restaurant 
+and we get an order for three meals of sushi and two pizzas.*/
+makePizza(name);
+makePizza(name);
+makeSushi(name);
+makeSushi(name);
+makeSushi(name);
+
+
+
