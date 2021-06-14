b6f79b41abe56662732ae3bf8a47adb8dfe13edd
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-14 13:26:56 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/Function return statements exercise/script.js
@@ -0,0 +1,30 @@
+
+function numberChecking(num1, num2) {
+      return num1 < num2 ;
+    }
+  console.log(numberChecking(3, 100));
+  //function type; boolean.
+
+const mayEnterClub = person => {
+  if (person.age < 18)
+    return false;
+  }
+  if (person.has_been_suspended){
+    return false;
+  }
+  if (clubVisitors > 20){
+    return false;
+  }
+  return true;
+
+  const person1 = {
+    name: "Baig" , age: 25, has_been_suspended: false, 
+  }
+  console.log(mayEnterClub(person1));
+
+  console.log(2+3)
+
+
+  
+
+
