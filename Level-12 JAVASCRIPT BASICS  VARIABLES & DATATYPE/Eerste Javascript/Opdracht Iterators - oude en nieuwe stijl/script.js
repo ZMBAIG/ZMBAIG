6750b7a15eb6031029996c34e2ac8996839e52c7
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-19 11:41:18 +0200

    Add files via upload

---
+++ Level-12 JAVASCRIPT BASICS  VARIABLES & DATATYPE/Eerste Javascript/Opdracht Iterators - oude en nieuwe stijl/script.js
@@ -0,0 +1,19 @@
+
+//Oude stijl: loops.
+
+const colors = ["yellow", "blue" , "red", "orange"]
+for (color of colors);
+  console.log(colors)
+
+//Nieuwe stijl: array methods
+
+const array_colors = ["yellow", "blue" , "red", "orange"];
+array_colors.forEach(element => console.log(element));
+
+
+  
+  
+
+
+
+
