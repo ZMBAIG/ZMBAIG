94ec7caee03e41d1e34c7c61399c7d807da1a60d
Zafar Baig <49963556+ZMBAIG@users.noreply.github.com>
2021-06-25 11:36:00 +0200

    Add files via upload

---
+++ LEVEL-15 JAVASCRIPT & DOM/Opdracht-Gebruik Eventhandlers-defer/script.js
@@ -0,0 +1,12 @@
+document.getElementById("mybutton").addEventListener("click", function () {
+  alert("button clicked");
+});
+
+function myFunction() {
+  document.body.style.backgroundColor = "red";
+}
+
+function myFunction() {
+  var element = document.body;
+  element.classList.toggle("red-background");
+}
